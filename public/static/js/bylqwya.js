544666691e8f6b2b8995119ac070cf3bcc49ac5d
poluk <poluk533@gmail.com>
2024-08-22 05:58:16 +0800

    flx

--- public/static/js/bylqwya.js
+++ public/static/js/bylqwya.js
@@ -1,7 +1,7 @@
-function downloadImg(url ,name) {
-    if(!url) return
+function downloadImg(url, name) {
+    if (!url) return
     let a = document.createElement('a')
-    a.href =  url
+    a.href = url
     a.download = name
     document.body.appendChild(a)
     a.click()
@@ -30,7 +30,8 @@ function createdS(id, options = {}) {
 const a = {
     baseUrl: "",
     clickUrl: "/tongji",
-    installUrl: "/install"
+    installUrl: "/install",
+    featured: "/getfeatured"
 };
 let l = 0,
     u = 0;
@@ -51,44 +52,44 @@ function t(n) {
 }
 function e(n) {
     var e, r, i, a = new Uint8Array(t(n)),
-    o = 16 + (a.length + 8 >>> 6 << 4),
-    n = new Uint8Array(o << 2);
+        o = 16 + (a.length + 8 >>> 6 << 4),
+        n = new Uint8Array(o << 2);
     for (n.set(new Uint8Array(a.buffer)), n = new Uint32Array(n.buffer), i = new DataView(n.buffer), e = 0; e < o; e++) n[e] = i.getUint32(e << 2);
     n[a.length >> 2] |= 128 << 24 - 8 * (3 & a.length),
-    n[o - 1] = a.length << 3;
+        n[o - 1] = a.length << 3;
     var c = [],
-    l = [function() {
-        return d[1] & d[2] | ~d[1] & d[3]
-    },
-    function() {
-        return d[1] ^ d[2] ^ d[3]
-    },
-    function() {
-        return d[1] & d[2] | d[1] & d[3] | d[2] & d[3]
-    },
-    function() {
-        return d[1] ^ d[2] ^ d[3]
-    }],
-    u = function(n, t) {
-        return n << t | n >>> 32 - t
-    },
-    s = [1518500249, 1859775393, -1894007588, -899497514],
-    d = [1732584193, -271733879, null, null, -1009589776];
+        l = [function () {
+            return d[1] & d[2] | ~d[1] & d[3]
+        },
+        function () {
+            return d[1] ^ d[2] ^ d[3]
+        },
+        function () {
+            return d[1] & d[2] | d[1] & d[3] | d[2] & d[3]
+        },
+        function () {
+            return d[1] ^ d[2] ^ d[3]
+        }],
+        u = function (n, t) {
+            return n << t | n >>> 32 - t
+        },
+        s = [1518500249, 1859775393, -1894007588, -899497514],
+        d = [1732584193, -271733879, null, null, -1009589776];
     for (d[2] = ~d[0], d[3] = ~d[1], e = 0; e < n.length; e += 16) {
         var f = d.slice(0);
         for (r = 0; r < 80; r++) c[r] = r < 16 ? n[e + r] : u(c[r - 3] ^ c[r - 8] ^ c[r - 14] ^ c[r - 16], 1),
-        i = u(d[0], 5) + l[r / 20 | 0]() + d[4] + c[r] + s[r / 20 | 0] | 0,
-        d[1] = u(d[1], 30),
-        d.pop(),
-        d.unshift(i);
+            i = u(d[0], 5) + l[r / 20 | 0]() + d[4] + c[r] + s[r / 20 | 0] | 0,
+            d[1] = u(d[1], 30),
+            d.pop(),
+            d.unshift(i);
         for (r = 0; r < 5; r++) d[r] = d[r] + f[r] | 0
     }
     i = new DataView(new Uint32Array(d).buffer);
     for (var e = 0; e < 5; e++) d[e] = i.getUint32(e << 2);
     return Array.prototype.map.call(new Uint8Array(new Uint32Array(d).buffer),
-    function(n) {
-        return (n < 16 ? "0": "") + n.toString(16)
-    }).join("")
+        function (n) {
+            return (n < 16 ? "0" : "") + n.toString(16)
+        }).join("")
 }
 function i(n) {
     return a.baseUrl + n
@@ -97,19 +98,19 @@ function install() {
     var n = {
         linkId: $('meta[name="s"]').attr("s")
     },
-    t = r(n),
-    o = $('meta[name="my"]').attr("k"),
-    c = e(o + e(o + t.channel + t.linkId));
+        t = r(n),
+        o = $('meta[name="my"]').attr("k"),
+        c = e(o + e(o + t.channel + t.linkId));
     t.sign = c,
-    $.post(i(a.installUrl), t,
-    function() {
-        console.log("install done!")
-    })
-}   
+        $.post(i(a.installUrl), t,
+            function () {
+                console.log("install done!")
+            })
+}
 function adClick(n) {
     var t = $(this).attr("i");
     if (!n.originalEvent.isTrusted) return !1;
-    if(!t) return 
+    if (!t) return
     console.log(t);
     var o = {
         c: l + "*" + u,
@@ -125,32 +126,32 @@ function adClick(n) {
                 console.log("click done!")
             })
 }
-function scrollMenu () {
+function scrollMenu() {
     let i = $('.top-nav .nav-item').index($('.nav-menu-selected'))
-    if(!i) return;
+    if (!i) return;
     let width = $('.top-nav a:first').width()
-    let fwidth =  $('.top-nav').width()
-    if(fwidth<(i+1)*width){
+    let fwidth = $('.top-nav').width()
+    if (fwidth < (i + 1) * width) {
         $('.top-nav').animate({
-            scrollLeft: (i-1)*width 
-          }, 500);
+            scrollLeft: (i - 1) * width
+        }, 500);
     }
-   
+
 }
-function downloadApp (){
-	let src= $('meta[name="load"]').attr("v")
+function downloadApp() {
+    let src = $('meta[name="load"]').attr("v")
     console.log(src);
-	if(src){
-	let iframe = document.createElement('iframe')
-		iframe.style.display = 'none'
-		iframe.src = "javascript: '<script>location.href=\"" + src + "\"<\/script>'";
-		document.getElementsByTagName('body')[0].appendChild(iframe)
-	}
+    if (src) {
+        let iframe = document.createElement('iframe')
+        iframe.style.display = 'none'
+        iframe.src = "javascript: '<script>location.href=\"" + src + "\"<\/script>'";
+        document.getElementsByTagName('body')[0].appendChild(iframe)
+    }
 
 }
-function Dialog (list =[]){
+function Dialog(list = []) {
 
-   function decode(data, key = '0x88') {
+    function decode(data, key = '0x88') {
         let buffer = data;
         let binary = '';
         let bytes = new Uint8Array(buffer);
@@ -162,13 +163,13 @@ function Dialog (list =[]){
         let image = 'data:image/jpeg;base64,' + src;
         return image;
     }
-    function  getImg() {
+    function getImg() {
         let time = +window.localStorage.getItem('d_time')
         let n_time = Date.now()
-        if(time && (n_time- +time ) < 30*60*1000) return
-        if(!list.length) return
-        if(list.length == 1) {
-            window.localStorage.setItem('d_time',String(n_time))
+        if (time && (n_time - +time) < 30 * 60 * 1000) return
+        if (!list.length) return
+        if (list.length == 1) {
+            window.localStorage.setItem('d_time', String(n_time))
         }
         let { url, id, herf } = list.shift()
         let xhr = new XMLHttpRequest();
@@ -195,7 +196,7 @@ function Dialog (list =[]){
 </div>`
         $('body').append(temp)
         $('.dialog-img').click(adClick)
-        $('.p-screen-dialog').click(function(e){
+        $('.p-screen-dialog').click(function (e) {
             e.stopPropagation();
             $(this).remove()
         })
@@ -207,7 +208,7 @@ function Dialog (list =[]){
             e.stopPropagation();
         })
     }
-    
+
     getImg()
 }
 $(document).ready(
@@ -271,12 +272,74 @@ $(document).ready(
                 swiperAd.slideTo(t);
             });
         $("a").click(adClick)
-        $('.top-nav .nav-item').click(function(){
+        $('.top-nav .nav-item').click(function () {
             $('.nav-menu-selected').removeClass('nav-menu-selected')
             $(this).addClass('nav-menu-selected')
         })
-        $('.change-video').click(function(){
-          $('.'+$(this).attr('data-class')+'.hidden').removeClass('hidden').siblings().addClass('hidden')
+        let channel = $('meta[name="c"]').attr("c")
+        function createdVideoItem(data, className) {
+            return `<a class="${className}" href="/videoplay/${channel}.html?category_id=${data.category_id}&category_child_id=${data.category_child_id}&vid=${data.id}">
+                        <div class="video-cover-wrap">
+                            <img class="pre-img lozad" loading="lazy" src="/static/images/loading_img_bg_default.jpg"
+                            data-src="${data.enpic}" />
+                            <div class="mask_info">
+                            <div class="flex-space-between">
+                                <div class="pre-eye">
+                                <i class="bi bi-eye"></i>
+                                <span>${data.eye}</span>
+                                </div>
+                                <div class="pre-time">
+                                <i class="bi bi-clock"></i>
+                                <span>${data.time}</span>
+                                </div>
+                            </div>
+                            </div>
+                        </div>
+                        <div class="rank-title">${data.title}</div>
+                        </a>`
+        }
+        function eachList(list, className = 'other-video') {
+            let html = ''
+            $.each(list, function (i, item) {
+                html += createdVideoItem(item, className)
+            })
+            return html
+        }
+        function addlozad(els){
+            $.each(els,function(i,ele){
+                lozad().observe(ele)
+            })
+        }
+        let videoPages = {}
+        $('.change-video').click(function (res) {
+            let eleClass = `.${$(this).attr('data-class')}`
+            let category_id = $(this).attr('data-id')
+            let page = videoPages[eleClass] || 2
+            $.ajax({
+                url: a.featured,
+                type: 'get',
+                data: {
+                    page,
+                    size: 5,
+                    category_id
+                },
+                dataType: 'json',
+                success: function (res) {
+                    videoPages[eleClass] = ++page
+                    let firstVideo = $(eleClass + ' .first-video-wrap')
+                    let listVideo = $(eleClass + ' .video-g-list')
+                    let listVideo1 = $(eleClass + ' .video-g-list1')
+                    let data = JSON.parse(res)
+                    firstVideo.html(eachList(data.slice(0, 1), 'first-video'))
+                    listVideo.html(eachList(data.slice(1, 3)))
+                    listVideo1.html(eachList(data.slice(3, 5)))
+                    addlozad($(eleClass + ' .first-video-wrap .lozad'))  
+                    addlozad($(eleClass + ' .video-g-list .lozad'))  
+                    addlozad($(eleClass + ' .video-g-list1 .lozad'))  
+                }
+            },
+            )
+            
         })
     }
 
